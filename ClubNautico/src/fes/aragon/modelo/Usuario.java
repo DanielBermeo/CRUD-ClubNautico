12eb1206566a46263404b14157d230c64073f4d3
DanielBermeo <83613856+DanielBermeo@users.noreply.github.com>
2024-03-22 19:01:20 -0600

    Subir archivos

---
+++ ClubNautico/src/fes/aragon/modelo/Usuario.java
@@ -0,0 +1,165 @@
+package fes.aragon.modelo;
+
+import java.sql.SQLException;
+
+import fes.aragon.repository.Conexion;
+
+public class Usuario {
+	
+	private int idContacto;
+	private String nombre, apPat, apMat,fullName, telefono,email;
+	private boolean socio, patron;
+	
+	
+	
+	
+	public Usuario(int idContacto, String nombre, String apPat, String apMat, String telefono, String email) {
+		super();
+		this.idContacto = idContacto;
+		this.nombre = nombre;
+		this.apPat = apPat;
+		this.apMat = apMat;
+		this.telefono = telefono;
+		this.email = email;
+		this.fullName = nombre +" " +apPat +" " + apMat;
+		setSocio();
+		setPatron();
+	}
+	
+	
+	
+	public Usuario(String nombre, String apPat, String apMat, String telefono, String email) {
+		super();
+		this.nombre = nombre;
+		this.apPat = apPat;
+		this.apMat = apMat;
+		this.telefono = telefono;
+		this.email = email;
+	}
+
+
+
+	public int getIdContacto() {
+		return idContacto;
+	}
+	public void setIdContacto(int idContacto) {
+		this.idContacto = idContacto;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApPat() {
+		return apPat;
+	}
+	public void setApPat(String apPat) {
+		this.apPat = apPat;
+	}
+	public String getApMat() {
+		return apMat;
+	}
+	public void setApMat(String apMat) {
+		this.apMat = apMat;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getFullName() {
+		return nombre +" " +apPat +" " + apMat;
+	}
+
+	
+	public String isSocio() {
+		if(socio) {
+			return "✔";
+		}else {
+			return "X";
+		}
+	}
+
+	public void setSocio() {
+	
+		try {
+			Conexion cnn = new Conexion();
+			this.socio = cnn.getExisteSocio(idContacto);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public String isPatron() {
+		if(patron) {
+			return "✔";
+		}else {
+			return "X";
+		}
+	}
+
+	public boolean daElPatron() {
+		return patron;
+	}
+	
+	public Usuario daElSocio() {
+		Usuario u = null;
+		try {
+			Conexion cnn = new Conexion();
+			if(socio) {
+				return this;
+			}else if(patron) {
+				u = cnn.getSocioDePatron(cnn.getIdPatron(this));
+			}
+			cnn.cerrarConexion();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return u;
+	}
+	
+	public void setPatron() {
+			
+		try {
+			Conexion cnn = new Conexion();
+			this.patron =  cnn.getExistePatron(idContacto);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return fullName;
+	}
+	
+	
+
+	
+}
